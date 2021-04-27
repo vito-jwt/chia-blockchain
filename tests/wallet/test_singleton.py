c43577559a526f6a2c9a232973b87a1816bb7e01
matt <matt@chia.net>
2021-04-27 18:25:59 +0100

    create p2_singleton puzzle and add test

--- tests/wallet/test_singleton.py
+++ tests/wallet/test_singleton.py
@@ -1,16 +1,18 @@
 from chia.wallet.puzzles.load_clvm import load_clvm
 from chia.types.blockchain_format.program import Program, INFINITE_COST
+from chia.types.announcement import Announcement
 
-DID_CORE_MOD = load_clvm("singleton_top_layer.clvm")
+SINGLETON_MOD = load_clvm("singleton_top_layer.clvm")
+P2_SINGLETON_MOD = load_clvm("p2_singleton.clvm")
 
 
 def test_only_odd_coins():
-    did_core_hash = DID_CORE_MOD.get_tree_hash()
+    did_core_hash = SINGLETON_MOD.get_tree_hash()
     solution = Program.to(
         [did_core_hash, did_core_hash, 1, [0xDEADBEEF, 0xCAFEF00D, 200], 200, [[51, 0xCAFEF00D, 200]]]
     )
     try:
-        result, cost = DID_CORE_MOD.run_with_cost(INFINITE_COST, solution)
+        cost, result = SINGLETON_MOD.run_with_cost(INFINITE_COST, solution)
     except Exception as e:
         assert e.args == ("clvm raise",)
     else:
@@ -19,13 +21,13 @@ def test_only_odd_coins():
         [did_core_hash, did_core_hash, 1, [0xDEADBEEF, 0xCAFEF00D, 210], 205, [[51, 0xCAFEF00D, 205]]]
     )
     try:
-        result, cost = DID_CORE_MOD.run_with_cost(INFINITE_COST, solution)
+        cost, result = SINGLETON_MOD.run_with_cost(INFINITE_COST, solution)
     except Exception:
         assert False
 
 
 def test_only_one_odd_coin_created():
-    did_core_hash = DID_CORE_MOD.get_tree_hash()
+    did_core_hash = SINGLETON_MOD.get_tree_hash()
     solution = Program.to(
         [
             did_core_hash,
@@ -37,7 +39,7 @@ def test_only_one_odd_coin_created():
         ]
     )
     try:
-        result, cost = DID_CORE_MOD.run_with_cost(INFINITE_COST, solution)
+        cost, result = SINGLETON_MOD.run_with_cost(INFINITE_COST, solution)
     except Exception as e:
         assert e.args == ("clvm raise",)
     else:
@@ -53,6 +55,24 @@ def test_only_one_odd_coin_created():
         ]
     )
     try:
-        result, cost = DID_CORE_MOD.run_with_cost(INFINITE_COST, solution)
+        cost, result = SINGLETON_MOD.run_with_cost(INFINITE_COST, solution)
     except Exception:
         assert False
+
+
+def test_p2_singleton():
+    singleton_mod_hash = SINGLETON_MOD.get_tree_hash()
+    genesis_id = 0xCAFEF00D
+    innerpuz = Program.to(1)
+    singleton_full = SINGLETON_MOD.curry(singleton_mod_hash, genesis_id, innerpuz)
+
+    p2_singleton_coin_id = Program.to(["test_hash"]).get_tree_hash()
+    expected_announcement = Announcement(singleton_full.get_tree_hash(), p2_singleton_coin_id).name()
+
+    p2_singleton_full = P2_SINGLETON_MOD.curry(
+        singleton_mod_hash, Program.to(singleton_mod_hash).get_tree_hash(), genesis_id
+    )
+    cost, result = p2_singleton_full.run_with_cost(
+        INFINITE_COST, Program.to([innerpuz.get_tree_hash(), p2_singleton_coin_id])
+    )
+    assert result.first().rest().first().as_atom() == expected_announcement
